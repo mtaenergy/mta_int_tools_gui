41baf0f5a6fa58907f69a739eb010273ebf3529d
ma-2pac <marcoalec@gmail.com>
2024-02-02 11:06:56 +1100

    add select nmi select box

--- app/pages/8_Forecasting.py
+++ app/pages/8_Forecasting.py
@@ -8,7 +8,13 @@ from streamlit import session_state
 from PIL import Image
 import mtatk
 
-from modules.utils import setup_session_states, measure_execution_time
+from modules.utils import setup_session_states, measure_execution_time, clear_flag, get_nmi_list
+
+## GLOBAL VARIABLES
+nmi_list =get_nmi_list()
+img_path = "app/imgs/400dpiLogo.jpg"
+session_state.live_state=0
+
 
 
 # Page: NMI Details
@@ -21,8 +27,19 @@ def forecasting_page():
         session_state.authenticator.logout("Logout","sidebar",key='unique_key')
         st.sidebar.title(f"Welcome {st.session_state['name']}")
 
+        #setup header
+        with st.container():
+                
+            col1, col2, col3 = st.columns(3)
+
+            st.title("Site Forecasts")      
+
+            with col2:
+                st.image(Image.open(img_path),use_column_width=True)
+
         
         #display search box for NMIs
+        nmi_in = st.selectbox("Select a NMI", nmi_list,on_change=clear_flag())
 
 
         # display graph of load forecast, predispatch cost and price forecast
